efff664e70c01261015af936fa99ca46f518da2c
axmonsalve <axel.monsalve.ponce@gmail.com>
2020-05-19 23:36:38 -0400

    Insert and get text from firebase

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const buttoms = document.querySelector('#botones'),
   userName = document.querySelector('#nombreUsuario'),
-  protectedContent = document.querySelector('#protected-content');
+  protectedContent = document.querySelector('#protected-content'),
+  chatForm = document.querySelector('#chat-form')
+inputChat = document.querySelector('#input-chat');
 
 // This is the OBSERVER
 firebase.auth().onAuthStateChanged((user) => {
@@ -16,7 +18,10 @@ firebase.auth().onAuthStateChanged((user) => {
 
     closeSession();
 
-    protectedContent.innerHTML = /*template*/ `<p class="text-center lead mt-5">Bienvenido/a ${user.email}</p>`;
+    //show chat form
+    chatForm.classList = 'input-group py-5 fixed-bottom container';
+
+    chatContent(user);
 
   } else {
 
@@ -28,6 +33,8 @@ firebase.auth().onAuthStateChanged((user) => {
     userName.innerHTML = '🗨 Chat';
 
     protectedContent.innerHTML = /*template*/ `<p class="text-center lead mt-5">Debes iniciar sesión</p>`;
+
+    chatForm.classList = 'input-group py-5 fixed-bottom container d-none';
 
   }
 });
@@ -49,4 +56,49 @@ const closeSession = () => {
   btnLogout.addEventListener('click', () => {
     firebase.auth().signOut();
   });
+};
+
+const chatContent = (user) => {
+  // protectedContent.innerHTML = /*template*/ `<p class="text-center lead mt-5">Bienvenido/a ${user.email}</p>`;
+
+  chatForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    if (!inputChat.value.trim()) {
+      console.log('input vacio');
+      return;
+    }
+
+    //Add element to our collection... This is a promise
+    firebase.firestore().collection('chat').add({
+        text: inputChat.value,
+        uid: user.uid,
+        date: Date.now()
+      })
+      .then(res => { console.log('mensaje guardado'); })
+      .catch(e => console.log(e));
+
+    //Clean input 
+    inputChat.value = '';
+  });
+
+  firebase.firestore().collection('chat').orderBy('date')
+    .onSnapshot(query => {
+
+      protectedContent.innerHTML = '';
+      query.forEach(doc => {
+        if (doc.data().uid === user.uid) {
+          protectedContent.innerHTML += /*template*/ `<div class="d-flex justify-content-end">
+          <span class="badge badge-pill badge-primary">${doc.data().text}</span>
+        </div>`;
+        } else {
+          protectedContent.innerHTML += /*template*/ `<div class="d-flex justify-content-start">
+          <span class="badge badge-pill badge-secondary">${doc.data().text}</span>
+        </div>`;
+        }
+
+        //Scroll on last message
+        protectedContent.scrollTop = protectedContent.scrollHeight;
+      });
+    });
 };
